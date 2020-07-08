3759dea4f40088427931b83bb047c21d306397ad
Sara Nilsen <sarabrandaoalves@gmail.com>
2020-07-08 14:46:09 -0700

    getting started with web midi

--- src/components/Piano.js
+++ src/components/Piano.js
@@ -1,30 +1,92 @@
-import React from "react";
+import React, { useState } from "react";
 import WebMidi from "webmidi";
 import "./Piano.css";
 
-const Piano = () => {
-  WebMidi.enable(function () {
+const Piano = (props) => {
+  //if user do not play a note for a minute pause the session
+  //last note played
+  //what if they start session and dont play any note
+  //if array empty for 60 sec -- pause session
+  //if array.length % 2 !== 0 in 60 seconds -- pause session
+  //[C4, C6]
+  const [notesPlayed, setNotesPlayed] = useState([]);
+
+  WebMidi.enable(function (err) {
+    if (err) {
+      console.warn(err);
+    } else {
+      console.log("Sysex is enabled!");
+    }
+  }, true);
+
+  WebMidi.enable(function (err) {
+    if (err) {
+      console.log("WebMidi could not be enabled.", err);
+    }
+
     // Viewing available inputs and outputs
     console.log(WebMidi.inputs);
     console.log(WebMidi.outputs);
+
+    // Display the current time
+    console.log(WebMidi.time);
+
+    const input = WebMidi.getInputByName("Xkey37");
+    console.log("INPUT", input);
+    input.addListener("noteon", "all", function (e) {
+      if (e.note.name === "C" && e.note.octave === 4) {
+      }
+
+      console.log(
+        "Received 'noteon' message (" + e.note.name + e.note.octave + ")."
+      );
+    });
   });
-  const octaves = 6;
-  const pianoKeys = () => {}; //
+
+  const pauseSession = () => {
+    props.pauseAuto();
+  };
+
+  const pianoKeys = () => {};
   return (
     <div>
       <ul className="set">
-        <li className="key white b"></li>
-        <li className="key black as"></li>
-        <li className="key white a"></li>
-        <li className="key black gs"></li>
-        <li className="key white g"></li>
+        <li className="key white f 2"></li>
         <li className="key black fs"></li>
-        <li className="key white f"></li>
-        <li className="key white e"></li>
-        <li className="key black ds"></li>
-        <li className="key white d"></li>
-        <li className="key black cs"></li>
+        <li className="key white g"></li>
+        <li className="key black gs"></li>
+        <li className="key white a"></li>
+        <li className="key black as"></li>
+        <li className="key white b"></li>
         <li className="key white c"></li>
+        <li className="key black cs"></li>
+        <li className="key white d"></li>
+        <li className="key black ds"></li>
+        <li className="key white e"></li>
+        <li className="key white f 3"></li>
+        <li className="key black fs"></li>
+        <li className="key white g"></li>
+        <li className="key black gs"></li>
+        <li className="key white a"></li>
+        <li className="key black as"></li>
+        <li className="key white b"></li>
+        <li className="key white c"></li>
+        <li className="key black cs"></li>
+        <li className="key white d"></li>
+        <li className="key black ds"></li>
+        <li className="key white e"></li>
+        <li className="key white f 4"></li>
+        <li className="key black fs"></li>
+        <li className="key white g"></li>
+        <li className="key black gs"></li>
+        <li className="key white a"></li>
+        <li className="key black as"></li>
+        <li className="key white b"></li>
+        <li className="key white c"></li>
+        <li className="key black cs"></li>
+        <li className="key white d"></li>
+        <li className="key black ds"></li>
+        <li className="key white e"></li>
       </ul>
     </div>
   );
--- src/components/timer/index.js
+++ src/components/timer/index.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Buttons from "./buttons";
 import formatDuration from "format-duration";
+import Piano from "../Piano";
 import "./styles.css";
 
 const DEFAULT_TIME = 15; // in minutes
@@ -8,6 +9,14 @@ const DEFAULT_TIME = 15; // in minutes
 const Timer = () => {
   const [isActive, setIsActive] = useState(false);
   const [elapsedTime, setElapsedTime] = useState(0);
+
+  //every second when active elapsed goes up
+  //if user do not play a note for a minute pause the session
+  //last note played
+  //what if they start session and dont play any note
+  //if array empty for 60 sec -- pause session
+  //if array.length % 2 !== 0 in 60 seconds -- pause session
+  //[C4, C6]
 
   useEffect(() => {
     let interval = null;
@@ -35,12 +44,17 @@ const Timer = () => {
     }
   };
 
+  const pauseAuto = () => {
+    setIsActive(false);
+  };
+
   return (
     <div className="container-timer">
       <div className="elapsed-time">
         {formatDuration(DEFAULT_TIME * 60 * 1000 - elapsedTime * 1000)}
       </div>
       <Buttons handleClick={onToggle} isActive={isActive} handleEnd={onEnd} />
+      <Piano pauseAuto={pauseAuto} />
     </div>
   );
 };
