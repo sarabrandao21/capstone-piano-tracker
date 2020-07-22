ef21b651fa0af7e429ec7ff216382abb90adbd98
Sara Nilsen <sarabrandaoalves@gmail.com>
2020-07-22 11:33:35 -0700

    total played in a specific day

--- src/components/DailyLog.js
+++ src/components/DailyLog.js
@@ -2,6 +2,7 @@ import React from "react";
 import { formatDurationWithSec } from "../util";
 import "./DailyLog.css";
 const DailyLog = ({ data }) => {
+  let totalPlayedForThatDay = 0;
   return (
     <div className="sessions-container">
       <h3>Your Session(s)</h3>
@@ -9,10 +10,21 @@ const DailyLog = ({ data }) => {
         {data ? (
           Object.keys(data).map((key) => {
             const value = data[key];
+            totalPlayedForThatDay += value.timePlayedInSeconds;
             return <li>{formatDurationWithSec(value.timePlayedInSeconds)}</li>;
           })
         ) : (
           <div>No sessions</div>
+        )}
+      </p>
+      <p>
+        {totalPlayedForThatDay > 0 ? (
+          <p>
+            Total:{" "}
+            <strong>{formatDurationWithSec(totalPlayedForThatDay)}</strong>
+          </p>
+        ) : (
+          ""
         )}
       </p>
     </div>
